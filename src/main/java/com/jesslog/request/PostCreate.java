7b7a283f51c078f023660030458c8fb06add9f52
Jess <nakedthunder00@gmail.com>
2023-10-12 05:52:53 +0900

    fix test code by ObjectMapper and Builder to readable

--- src/main/java/com/jesslog/request/PostCreate.java
+++ src/main/java/com/jesslog/request/PostCreate.java
@@ -1,5 +1,6 @@
 package com.jesslog.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,4 +15,11 @@ public class PostCreate {
     private String title;
     @NotBlank(message = "컨텐츠를 입력해주세요.")
     private String content;
+
+    // 생성자 생성 , @AllArgucontr...
+    @Builder // java 디자인패턴, 빌더패턴
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/test/java/com/jesslog/controller/PostControllerTest.java
+++ src/test/java/com/jesslog/controller/PostControllerTest.java
@@ -1,19 +1,30 @@
 package com.jesslog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jesslog.repository.PostRepository;
+import com.jesslog.request.PostCreate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@WebMvcTest
+@AutoConfigureMockMvc
+@SpringBootTest
 class PostControllerTest {
 
     @Autowired
@@ -22,12 +33,33 @@ class PostControllerTest {
     @Autowired // 필드주입으로 받는다.
     private PostRepository postRepository;
 
+    @BeforeEach
+    void clean(){
+        postRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("/post 요청 시 Hello world 출력")
     void postTest() throws Exception {
+        // PostCreate 클래스에서 생성자를 만든것을 담아준다.
+        // 나중에 다른 개발자가 생성자 필드를 바꾼다면? ... 나중에 버그를 발견하기 어려워짐
+        // 원인 찾기가 어려워져서,
+        //PostCreate request = new PostCreate("제목입니다.", "내용입니다.");
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        // 잭슨 프로세싱해주는 라이브러리
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(request); // getter가 자바빈규약룰을따라서 json형태로 가공을 해준다.
+
+        // 생성자로 통해 넣은 값들이 ObjectMapper를 통해 String으로 잘 들어갔는지 확인하기위해
+        System.out.println(json);
+
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("{}"))
@@ -38,7 +70,7 @@ class PostControllerTest {
     @DisplayName("/post 요청 시 title 빈값이면 안됨")
     void titleNotNullTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"title\": null, \"content\": \"내용입니다.\"}")
                 )
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
@@ -52,14 +84,15 @@ class PostControllerTest {
     @DisplayName("/post 요청 시 DB에 값이 저장된다.")
     void insertToDbByPost() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"title\": \"제목 입니다.\", \"content\": \"내용입니다.\"}")
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
 
-        // then, 요청한 json이 DB에 저장되기 때문에 
-
+        // then, 요청한 json이 DB에 저장되기 때문에
+        // Assertions.assertEquals()
+        assertEquals(1L, postRepository.count());
 
     }
 }
